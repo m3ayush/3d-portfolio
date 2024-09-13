1425f70ea520e4940399f07baec667f9ccb5a392
m3ayush <ayushbhat0518@gmail.com>
2024-09-13 23:39:48 +0530

    hero file name to Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
-import Hero from './sections/hero'
+import Hero from './sections/Hero'
 import About from './sections/About'
 import Projects from './sections/Projects'
 import Clients from './sections/Clients'
